5770c02b887b750220f57bb7d733fac8716e7a64
Chappo588 <76132938+Chappo588@users.noreply.github.com>
2020-12-31 14:47:34 +0300

    Proje oluşturuldu

---
+++ ProjeWeb/ProjeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
